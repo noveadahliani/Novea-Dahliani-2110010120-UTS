0402a0d14321c6677678e261b389307c15ea7e91
noveadahliani <134039826+noveadahliani@users.noreply.github.com>
2023-11-25 18:59:13 +0700

    Add files via upload

---
+++ AplikasiInventarisBarang/AplikasiInventarisBarang/src/inventarisbarang/model.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventarisbarang;
+
+/**
+ *
+ * @author User
+ */
+class model {
+
+    static void addRow(Object[] object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void removeRow(int selectedRowForDeletion) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Object getValueAt(int selectedRow, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static class addRow {
+
+        public addRow() {
+        }
+    }
+    
+}
